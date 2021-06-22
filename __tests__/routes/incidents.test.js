20cf8a5e5c0b140d70fea14cbcad3a6559308461
krsnyder <kirk.robert.snyder@gmail.com>
2021-06-22 10:07:27 -0400

    wip

--- __tests__/routes/incidents.test.js
+++ __tests__/routes/incidents.test.js
@@ -3,6 +3,11 @@ const db = require('../../data/db-config');
 const server = require('../../server');
 const Incidents = require('../../api/incidents/incidentsModel');
 
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
 it('correct env var', () => {
   expect(process.env.NODE_ENV).toBe('test');
 });
